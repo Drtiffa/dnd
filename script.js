0792e2fe96efd18e07176d0e5fc5ceebe336ac11
Tiffany Drouet <tiffanydrouet96@gmail.com>
2022-04-19 23:50:00 +0200

    Style lines add blocks

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const buttonAdd = document.getElementById("add");
 let i = 0;
 
-buttonAdd.addEventListener("click", () => {
-    const blockWrapper = document.getElementById("block-wrapper");
+buttonAdd.addEventListener("click", (e) => {
+    const blockChildren = document.getElementById("children");
+    const parentNumber = e.target.parentElement.getAttribute('data-parent');
     i += 1;
 
-    if(blockWrapper.childElementCount < 4) {
-        blockWrapper.insertAdjacentHTML("beforeend", "<div class='block child"+i+"'><p>child "+i+"</p><button id='add_"+i+"'>+</button></div>");
+    blockChildren.classList.add("has-child");
+
+    if(blockChildren.childElementCount === 1) {
+        blockChildren.classList.add("has-two-child");
+    } else if (blockChildren.childElementCount === 2) {
+        blockChildren.classList.add("has-three-child");
+    }
+
+    if(blockChildren.childElementCount < 3) {
+        blockChildren.insertAdjacentHTML("beforeend", "<div class='block child_"+i+"' data-child='"+parentNumber+"_"+i+"'><p>child "+parentNumber+"_"+i+"</p><button id='add_"+parentNumber+"_"+i+"'>+</button></div>");
     }
 });
