2178aaa1f6c8cf77ee09c87ec15ba2db1dc39520
Tiffany Drouet <tiffanydrouet96@gmail.com>
2022-04-24 23:21:23 +0200

    Add begining test for generalize click add child

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const buttonAdd = document.getElementById("add");
+const buttonAdd = document.querySelectorAll('*[id^="add"]');
+const blockChildren = document.getElementById("children");
 let i = 0;
 
+console.log(buttonAdd)
+
 buttonAdd.addEventListener("click", (e) => {
-    const blockChildren = document.getElementById("children");
-    const parentNumber = e.target.parentElement.getAttribute('data-parent');
+    const parentNumber = e.target.parentElement.getAttribute("data-parent");
     i += 1;
 
     blockChildren.classList.add("has-child");
 
-    if(blockChildren.childElementCount === 1) {
+    if (blockChildren.childElementCount === 1) {
         blockChildren.classList.add("has-two-child");
+
     } else if (blockChildren.childElementCount === 2) {
         blockChildren.classList.add("has-three-child");
+        buttonAdd.remove();
     }
 
-    if(blockChildren.childElementCount < 3) {
+    if (blockChildren.childElementCount < 3) {
         blockChildren.insertAdjacentHTML("beforeend", "<div class='block child_"+i+"' data-child='"+parentNumber+"_"+i+"'><p>child "+parentNumber+"_"+i+"</p><button id='add_"+parentNumber+"_"+i+"'>+</button></div>");
     }
 });
