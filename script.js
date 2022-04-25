67d0373a743f547f3d0b7da50c7aa31a60b3825c
Tiffany Drouet <tiffanydrouet96@gmail.com>
2022-04-25 23:56:36 +0200

    Add children for first level child

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const buttonAdd = document.querySelectorAll('*[id^="add"]');
-const blockChildren = document.getElementById("children");
+const buttonAdd = document.getElementById("add");
+const blockChildren = document.querySelector(".children");
 let i = 0;
 
-console.log(buttonAdd)
-
+// first parent click
 buttonAdd.addEventListener("click", (e) => {
     const parentNumber = e.target.parentElement.getAttribute("data-parent");
     i += 1;
@@ -19,6 +18,40 @@ buttonAdd.addEventListener("click", (e) => {
     }
 
     if (blockChildren.childElementCount < 3) {
-        blockChildren.insertAdjacentHTML("beforeend", "<div class='block child_"+i+"' data-child='"+parentNumber+"_"+i+"'><p>child "+parentNumber+"_"+i+"</p><button id='add_"+parentNumber+"_"+i+"'>+</button></div>");
+        blockChildren.insertAdjacentHTML("beforeend", "<div id='block_"+[parseInt(parentNumber)+1]+"' class='family'><div class='block child child_"+i+"' data-child='"+[parseInt(parentNumber)+1]+"_"+i+"'><p>child "+parentNumber+"_"+i+"</p><button id='add_"+parentNumber+"_"+i+"'>+</button></div><div class='children'></div></div>");
+    }
+
+    // first child click
+    let children = document.querySelectorAll('button')
+    if (children) {
+        let i = 0;
+
+        children.forEach(function(element) {
+            element.addEventListener('click', function(e) {
+                const parentNumber = e.target.parentElement.getAttribute('data-child');
+                
+                console.log(parentNumber)
+                
+                let blockChild = document.querySelector('#block_'+parseInt(parentNumber)+' .children');
+
+                console.log(blockChild)
+
+                i += 1;
+
+                blockChild.classList.add("has-child");
+
+                if (blockChild.childElementCount === 1) {
+                    blockChild.classList.add("has-two-child");
+
+                } else if (blockChild.childElementCount === 2) {
+                    blockChild.classList.add("has-three-child");
+                    buttonAdd.remove();
+                }
+
+                if (blockChild.childElementCount < 3) {
+                    blockChild.insertAdjacentHTML("beforeend", "<div id='block_"+[parseInt(parentNumber)+1]+"' class='family'><div class='block child child_"+i+"' data-child='"+parentNumber+"_"+i+"'><p>child "+parentNumber+"_"+i+"</p><button id='add_"+parentNumber+"_"+i+"'>+</button></div><div class='children'></div></div>");
+                }
+            })
+        })
     }
 });
